7ac6db6cc43eeb91b9b89b2c9068d228ceebe39d
RainerYuan <36510771+RainerYuan@users.noreply.github.com>
2018-05-02 19:36:08 -0500

    Update Team.java

--- Team.java
+++ Team.java
@@ -4,24 +4,24 @@ package application;
  * It contains the name and rank (for seeding) of the team.
  */
 public class Team {
-	private String name;
-	private int rank;
-	/**
-	 *  getter for initial rank of the team
-	 * 	setter for round is not required because it is set only once when the constructor is called
-	 *	according to the position in the file
-	 */
-	public Team(String name, int rank) {
-		this.name = name;
-		this.rank = rank;
-	}
-	public int getRank() {
-		return rank;
-	}
-	/**
-	 *  getter for round
-	 */
-	public String getName() {
-		return name;
-	}
+    private String name;
+    private int rank;
+    /**
+     *  getter for initial rank of the team
+     *  setter for round is not required because it is set only once when the constructor is called
+     *  according to the position in the file
+     */
+    public Team(String name, int rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+    public int getRank() {
+        return rank;
+    }
+    /**
+     *  getter for round
+     */
+    public String getName() {
+        return name;
+    }
 }
