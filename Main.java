f5540fe2d454332123c50bfc3097faf025c8cc69
Combustib1eLemon <37025108+Combustib1eLemon@users.noreply.github.com>
2018-05-03 18:22:45 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,20 +12,8 @@
 package application;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-
 
 public class Main extends Application {
     Stage window; // The application window that holds the GUI
