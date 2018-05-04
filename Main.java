f9a17c689d081c2bd99cca43943a9b3747a7959c
Combustib1eLemon <37025108+Combustib1eLemon@users.noreply.github.com>
2018-05-03 19:47:42 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,21 +13,10 @@ package application;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
